cc084e6b669944f5ba755aba58e4ce72078ae9c9
Diallo Dev <saifoulaye.diallodev@gmail.com>
2024-06-15 16:44:59 +0000

    refactor: ajuste experience and education section
    - add organization logo
    - reorganize content

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -12,28 +12,69 @@ const Resume = () => {
 
             <div className="resume-widget">
               <div className="resume-item wow fadeInLeft" data-wow-delay=".4s">
-                <h3 className="resume-title">Software Developer - Consultant</h3>
-                <div className="time">Nov 2023 - Present</div>
-                <div className="institute">
-                  ENG Technologie Group, Sicap Baobab - Dakar
+                <div className="resume-item-left">
+                  <img
+                    src="/src/assets/img/icons/eng_tech_group_logo.jpeg"
+                    alt=""
+                    width={48}
+                  />
+                </div>
+                <div className="resume-item-right">
+                  <h3 className="role">Software Developer - Consultant</h3>
+                  <div className="organization">
+                    ENG Technologie Group - Contract
+                  </div>
+                  <div className="time">Nov 2023 - Present</div>
+                  <div className="location">Dakar, Senegal - Full-time</div>
                 </div>
               </div>
               <div className="resume-item wow fadeInLeft" data-wow-delay=".5s">
-                <h3 className="resume-title">Software Developer Intern</h3>
-                <div className="time">July 2023 - Oct 2023 • 4 mois</div>
-                <div className="institute">
-                  ENG Technologie Group, Sicap Baobab - Dakar
+                <div className="resume-item-left">
+                  <img
+                    src="/src/assets/img/icons/eng_tech_group_logo.jpeg"
+                    alt=""
+                    width={48}
+                  />
+                </div>
+                <div className="resume-item-right">
+                  <h3 className="role">Software Developer Intern</h3>
+                  <div className="organization">
+                    ENG Technologie Group - Stage
+                  </div>
+                  <div className="time">July 2023 - Oct 2023 • 4 mois</div>
+                  <div className="location">Dakar, Senegal - Full-time</div>
                 </div>
               </div>
               <div className="resume-item wow fadeInLeft" data-wow-delay=".6s">
-                <h3 className="resume-title">Software Engineer</h3>
-                <div className="time">{"Nov 2023 - Present"}</div>
-                <div className="institute">Mafalia, Dakar</div>
+                <div className="resume-item-left">
+                  <img
+                    src="/src/assets/img/icons/mafalia_logo.jpeg"
+                    alt=""
+                    width={48}
+                  />
+                </div>
+                <div className="resume-item-right">
+                  <h3 className="role">Software Engineer</h3>
+                  <div className="organization">Mafalia - Contract</div>
+                  <div className="time">{"Nov 2023 - Present"}</div>
+                  <div className="location">Dakar, Senegal - Remote</div>
+                </div>
               </div>
               <div className="resume-item wow fadeInLeft" data-wow-delay=".7s">
-                <h3 className="resume-title">Full Stack Web Developer</h3>
+              <div className="resume-item-left">
+                  <img
+                    src="/src/assets/img/icons/sarayatech_logo.jpeg"
+                    alt=""
+                    width={48}
+                  />
+                </div>
+                <div className="resume-item-right">
+                  <h3 className="role">Full Stack Web Developer</h3>
+                  <div className="organization">SarayaTech Senegal - Stage</div>
                 <div className="time">Oct 2021 - Dec 2022 • 3 mois</div>
-                <div className="institute">SarayaTech Senegal, Dakar</div>
+                <div className="location">Dakar, Senegal</div>
+                </div>
+                
               </div>
             </div>
           </div>
@@ -46,24 +87,22 @@ const Resume = () => {
             </div>
 
             <div className="resume-widget">
-              <div className="resume-item wow fadeInRight" data-wow-delay=".5s">
+              <div className="edu-item wow fadeInRight" data-wow-delay=".5s">
                 <div className="time">2020 - 2023</div>
-                <h3 className="resume-title">Programming course</h3>
+                <h3 className="edu-title">Programming course</h3>
                 <div className="institute">Harverd University</div>
               </div>
-              {/* <div className="resume-item wow fadeInRight" data-wow-delay=".6s">
-                <div className="time">2016 - 2020</div>
-                <h3 className="resume-title">Graphic design course</h3>
-                <div className="institute">University of Denmark</div>
-              </div> */}
-              <div className="resume-item wow fadeInRight" data-wow-delay=".7s">
+            
+              <div className="edu-item wow fadeInRight" data-wow-delay=".7s">
                 <div className="time">2022 - 2023</div>
-                <h3 className="resume-title">Certificate Java Full-Stack Developer</h3>
+                <h3 className="edu-title">
+                  Certificate Java Full-Stack Developer
+                </h3>
                 <div className="institute">SarayaTech Senegal, Dakar</div>
               </div>
-              <div className="resume-item wow fadeInRight" data-wow-delay=".8s">
+              <div className="edu-item wow fadeInRight" data-wow-delay=".8s">
                 <div className="time">2020 - 2023</div>
-                <h3 className="resume-title">
+                <h3 className="edu-title">
                   Bachelor Degree - Software Development
                 </h3>
                 <div className="institute">
